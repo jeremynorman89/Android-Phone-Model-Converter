d1f96a7ed9573ed0d718ef3774c803986897adce
Jeremy Norman <jeremy.norman@wearespindle.com>
2021-06-16 02:31:19 +0100

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
     <title>Android Phone Model Converter</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @livewireStyles
